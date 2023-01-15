8065295942577ce81ac3f1a9827d14057fc2008c
Moritz ‎‎‎‎‎‎‎‎ <moluecke@gmx.de>
2023-01-15 13:42:37 +0100

    Selection sort

--- src/array.rs
+++ src/array.rs
@@ -2,28 +2,67 @@ use sfml::graphics::{RenderWindow, RectangleShape, Transformable, Shape, Color, 
 use rand::Rng;
 
 
-pub fn generate(len: usize) -> Vec<u16> {
+#[derive(Clone)]
+pub enum State {
+    Sorted,
+    NotSorted,
+    Selected,
+}
+impl State {
+    pub fn color(&self) -> Color {
+        match self {
+            State::Sorted => Color::GREEN,
+            State::NotSorted => Color::WHITE,
+            State::Selected => Color::YELLOW,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct Elm {
+    pub val: u16,
+    pub state: State
+}
+impl Elm {
+    pub fn new(val: u16) -> Self {
+        Self { val, state: State::NotSorted }
+    }
+}
+
+
+pub fn random(len: usize) -> Vec<Elm> {
     let mut rng = rand::thread_rng();
     let mut array = Vec::with_capacity(len);
     for _ in 0..len {
-        array.push(rng.gen_range(0..101));
+        array.push(Elm::new(rng.gen_range(0..101)));
     }
     array
 }
 
+pub fn reversed(len: usize) -> Vec<Elm> {
+    let mut array = Vec::with_capacity(len);
+    for i in 0..len {
+        array.push(Elm::new((i as u16 + 1) * 10));
+    }
 
-pub fn draw(win: &mut RenderWindow, win_size: (f32, f32), bar_spacing: f32, array: &[u16]) {
+    array.reverse();
+    array
+}
+
+
+
+pub fn draw(win: &mut RenderWindow, win_size: (f32, f32), bar_spacing: f32, array: Vec<Elm>) {
     let bar_num = array.len() as f32;
     let bar_width = (win_size.0 - (bar_num + 1.) * bar_spacing) / bar_num;
     let mut x: f32 = bar_spacing;
 
     for elm in array {
         let mut bar = RectangleShape::new();
-        let bar_height = win_size.1 * *elm as f32 / 100.;
+        let bar_height = win_size.1 * elm.val as f32 / 100.;
 
         bar.set_size((bar_width as f32, bar_height));
         bar.set_position((x, win_size.1 - bar_height));
-        bar.set_fill_color(Color::WHITE);
+        bar.set_fill_color(elm.state.color());
         win.draw(&bar);
 
         x += bar_width + bar_spacing;
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod algorithm;
 
 use sfml::{
     graphics::{RenderWindow, RenderTarget, Color},
-    window::{Style, Event}
+    window::{Style, Event, Key}
 };
 
 
@@ -12,15 +12,14 @@ fn main() {
     let mut win = RenderWindow::new((800, 600), "Sorting algorithms", Style::CLOSE, &Default::default());
     win.set_framerate_limit(144);
     
-    let arr = array::generate(100);
-    
-    println!("");
+    let mut sort = algorithm::Selection::new(array::reversed(10));
 
     // Main loop
     while win.is_open() {
         while let Some(event) = win.poll_event() {
             match event {
                 Event::Closed => win.close(),
+                Event::KeyPressed { code, alt: _, ctrl: _, shift: _, system: _ } if code == Key::Space => sort.step(),
                 _ => ()
             }
         }
@@ -28,7 +27,7 @@ fn main() {
         // Algorithm step
         
         win.clear(Color::BLACK);
-        array::draw(&mut win, (800., 600.), 2., &arr[..]);
+        array::draw(&mut win, (800., 600.), 2., sort.arr.clone());
         win.display();
     }
 }
